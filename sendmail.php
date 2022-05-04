592d15323cf88577d0a384cf4084292ec5d51508
Konstantin Sinitsyn <68946932+Konstantin-Sinitsyn@users.noreply.github.com>
2022-05-04 10:23:59 +0300

    added registration
    registration of participants

--- sendmail.php
+++ sendmail.php
@@ -1,72 +1,72 @@
-<?php
-// Файлы phpmailer
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
-require 'phpmailer/src/Exception.php';
+<?php 
 
-// Переменные, которые отправляет пользователь
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
 $fio = $_POST['fio'];
 $email = $_POST['email'];
-$text = $_POST['text'];
-$fileArticle = $_FILES['article'];
-$fileAnnotation = $_FILES['annotation'];
-$fileExpertise = $_FILES['expertise'];
+$tel = $_POST['tel'];
+$organization = $_POST['organization'];
+$city = $_POST['city'];
+$jobTitle = $_POST['jobTitle'];
+$academicDegree = $_POST['academicDegree'];
+$typeParticipation = $_POST['typeParticipation'];
+$articleTitleRU = $_POST['articleTitleRU'];
+$articleTitleEN = $_POST['articleTitleEN'];
+$authorInformationRU = $_POST['authorInformationRU'];
+$authorInformationEN = $_POST['authorInformationEN'];
+$organizationCityAuthorsRU = $_POST['organizationCityAuthorsRU'];
+$organizationCityAuthorsEN = $_POST['organizationCityAuthorsEN'];
+$hotel = $_POST['hotel'];
+$bookingDate = $_POST['bookingDate'];
 
-// Формирование самого письма
-$title = "Заявка с сайта FREME";
-$body = "
-<h2>Новый участник конференции $fio</h2>
-<b>Имя:</b> $fio<br>
-<b>Почта:</b> $email<br>
-<b>Сообщение:</b><br>$text
-";
 
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
-    $mail->isSMTP();   
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
-    //$mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
-    $mail->Username   = 'freme.2022@mail.ru'; // Логин на почте
-    $mail->Password   = 'fBkA2HLT7fc2ybJXYcM0'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('freme.2022@mail.ru', 'Заявка с сайта'); // Адрес самой почты и имя отправителя
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'freme.info@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'ZsqUNMYYmrhFTQvjzyzJ'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-    // Получатель письма
-    $mail->addAddress('freme.2022@mail.ru');  
+$mail->setFrom('freme.info@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('freme.2022@mail.ru');     // Кому будет уходить письмо 
+$mail->addAttachment($_FILES['article']['tmp_name'], $_FILES['article']['name']);
+$mail->addAttachment($_FILES['annotation']['tmp_name'], $_FILES['annotation']['name']); 
+$mail->addAttachment($_FILES['expertise']['tmp_name'], $_FILES['expertise']['name']);
+$mail->addAttachment($_FILES['check']['tmp_name'], $_FILES['check']['name']);     // Optional name
+$mail->isHTML(true);                                    // Set email format to HTML
 
-    // Прикрипление файлов к письму
-if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-        $filename = $file['name'][$ct];
-        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-            $mail->addAttachment($uploadfile, $filename);
-            $rfile[] = "Файл $filename прикреплён";
-        } else {
-            $rfile[] = "Не удалось прикрепить файл $filename";
-        }
-    }   
+$mail->Subject = 'Заявка с сайта Freme от '.$fio;
+$mail->Body    = '
+<h2>Новый участник конференции</h2><br>
+<b>АВТОР УЧАСТНИК (ФИО):</b> '.$fio.'<br>
+<b>АДРЕС ЭЛЕКТРОННОЙ ПОЧТЫ:</b> '.$email.'<br>
+<b>ТЕЛЕФОН:</b> '.$tel.'<br>
+<b>ОРГАНИЗАЦИЯ:</b> '.$organization.'<br>
+<b>ГОРОД:</b> '.$city.'<br>
+<b>ДОЛЖНОСТЬ:</b> '.$jobTitle.'<br>
+<b>УЧЁНАЯ СТЕПЕНЬ/ЗВАНИЕ:</b> '.$academicDegree.'<br>
+<b>НАЗВАНИЕ СТАТЬИ(RU):</b> '.$articleTitleRU.'<br>
+<b>НАЗВАНИЕ СТАТЬИ(EN):</b> '.$articleTitleEN.'<br>
+<b>ИНФОРМАЦИЯ ОБ АВТОРАХ(RU):</b> '.$authorInformationRU.'<br>
+<b>ИНФОРМАЦИЯ ОБ АВТОРАХ(EN):</b> '.$authorInformationEN.'<br>
+<b>ОРГАНИЗАЦИЯ/ГОРОД АВТОРОВ(RU):</b> '.$organizationCityAuthorsRU.'<br>
+<b>ОРГАНИЗАЦИЯ/ГОРОД АВТОРОВ(EN):</b> '.$organizationCityAuthorsEN.'<br>
+<b>ВИД УЧАСТИЯ:</b> '.$typeParticipation.'<br>
+<b>Нужна ли гостиница:</b> '.$hotel.'<br>
+<b>Даты бронирование гостиницы:</b> '.$bookingDate.'<br>
+';
+
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    header('location: popup.html');
+    echo 'Error';
+} else {
+    header('location: sent.html');
 }
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
-
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";} 
-else {$result = "error";}
-
-} catch (Exception $e) {
-    $result = "error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
-}
-
-// Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+?>
